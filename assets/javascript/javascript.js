9a9d343b3693c2bee21f551b7ec880d4067c16bf
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-01-21 18:02:25 -0800

    Finished Coding questions.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,27 @@
-// Added variables
+// Coding Questions
+var questions = [{
+    question: "What of the following options is used to return the first element within the document that is selected?"
+    options: ["addElement( )", "parseInt ( )", "querySelector ( )", "appendChild ( )"
+    answer: "querySelector( )"
+},
+{
+    title: "Who created JavaScript?",
+    question: ["Brendan Eich( )", "Håkon Wium Lie( )", "Tim Berners-Lee( )", "John Resig( )"],
+    answer: "Brendan Eich( )"
+},
+{
+    question: "When counting the indexes in an array, what number do you start with?",
+    options: ["3( )", "1( )", "2( )", "0()"],
+    answer: "0( )"
+},
+{
+    question: "Which of the following options is a boolean value?",
+    options: ["Ok( )", "function( )", "var( )", "false( )"],
+    answer: "false( )"
+},
+{
+    question: "Which of the following is a string method?",
+    options: ["height( )", "length( )", "console.log( )", "None of the above.( )"],
+    answer: "length( )"
+}
+]
