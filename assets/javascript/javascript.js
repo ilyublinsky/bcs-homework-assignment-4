b85ad0f82457aa39d559fb3ea5d9196fe966b37c
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-01-21 22:43:05 -0800

    final styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,150 +1,153 @@
-// Coding Questions
+//Coding questions    
 var questions = [{
-    question: "What of the following options is used to return the first element within the document that is selected?"
-    options: ["addElement( )", "parseInt ( )", "querySelector ( )", "appendChild ( )"
+    title: "What of the following options is used to return the first element within the document that is selected?",
+    choices: ["addElement( )", "parseInt ( )", "querySelector ( )", "appendChild ( )"],
     answer: "querySelector( )"
 },
 {
     title: "Who created JavaScript?",
-    question: ["Brendan Eich( )", "Håkon Wium Lie( )", "Tim Berners-Lee( )", "John Resig( )"],
+    choices: ["Brendan Eich( )", "Håkon Wium Lie( )", "Tim Berners-Lee( )", "John Resig( )"],
     answer: "Brendan Eich( )"
 },
 {
-    question: "When counting the indexes in an array, what number do you start with?",
-    options: ["3( )", "1( )", "2( )", "0()"],
+    title: "When counting the indexes in an array, what number do you start with?",
+    choices: ["3( )", "1( )", "2( )", "0()"],
     answer: "0( )"
 },
 {
-    question: "Which of the following options is a boolean value?",
-    options: ["Ok( )", "function( )", "var( )", "false( )"],
+    title: "Which of the following options is a boolean value?",
+    choices: ["Ok( )", "function( )", "var( )", "false( )"],
     answer: "false( )"
 },
 {
-    question: "Which of the following is a string method?",
-    options: ["height( )", "length( )", "console.log( )", "None of the above.( )"],
+    title: "Which of the following is a string method?",
+    choices: ["height( )", "length( )", "console.log( )", "None of the above.( )"],
     answer: "length( )"
 }
 ]
 
-// Score and timer functions
-
+//Score & timer values
 var score = 0;
 var currentQuestion = -1;
 var timeLeft = 0;
 var timer;
 
-// Starts the countdown timer once user clicks the 'start' button
+//Timer initiation after clicking start button
 function start() {
 
-    timeLeft = 75;
+timeLeft = 75;
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+timer = setInterval(function() {
+    timeLeft--;
     document.getElementById("timeLeft").innerHTML = timeLeft;
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        endGame(); 
+    }
+}, 1000);
 
-    timer = setInterval(function() {
-        timeLeft--;
-        document.getElementById("timeLeft").innerHTML = timeLeft;
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            endGame(); 
-        }
-    }, 5000);
-
-    next();
+next();
 }
 
-// Function to end game when the timer is done.
+//stops the timer to end game
 function endGame() {
-    clearInterval(timer);
+clearInterval(timer);
 
-    var quizContent = `
-    <h2>Time's up!</h2>
-    <h3>You got a ` + score +  ` /100!</h3>
-    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
-    <input type="text" id="name" placeholder="Initials"> 
-    <button onclick="setScore()">Set score!</button>`;
+var quizContent = `
+<h2>Game over!</h2>
+<h3>You got a ` + score +  ` /5!</h3>
+<h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+<input type="text" id="name" placeholder="Add your initials here!"> 
+<button onclick="setScore()">Set score!</button>`;
 
-    document.getElementById("quizBody").innerHTML = quizContent;
+document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//store the scores on local storage
+//adds on to local storage
 function setScore() {
-    localStorage.setItem("highscore", score);
-    localStorage.setItem("highscoreName",  document.getElementById('name').value);
-    getScore();
+localStorage.setItem("highscore", score);
+localStorage.setItem("highscoreName",  document.getElementById('name').value);
+getScore();
 }
+
 
 function getScore() {
-    var quizContent = `
-    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
-    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
-    
-    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
-    
-    `;
+var quizContent = `
+<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h1>` + localStorage.getItem("highscore") + `</h1><br> 
 
-    document.getElementById("quizBody").innerHTML = quizContent;
+<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+
+`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//Clears score
+//clears score
 function clearScore() {
-    localStorage.setItem("highscore", "");
-    localStorage.setItem("highscoreName",  "");
+localStorage.setItem("highscore", "");
+localStorage.setItem("highscoreName",  "");
 
-    resetQuiz();
+resetGame();
 }
 
 //reset the game 
 function resetGame() {
-    clearInterval(timer);
-    score = 0;
-    currentQuestion = -1;
-    timeLeft = 0;
-    timer = null;
+clearInterval(timer);
+score = 0;
+currentQuestion = -1;
+timeLeft = 0;
+timer = null;
 
-    document.getElementById("timeLeft").innerHTML = timeLeft;
+document.getElementById("timeLeft").innerHTML = timeLeft;
 
-    var quizContent = `
-    <h2>
-        Code Quiz
-    </h2>
-    <button onclick="start()">Start!</button>`;
+var quizContent = `
+<h1>
+     - Code Quiz -
+</h1>
+<h3>
+    Click to the button below to play!   
+</h3>
+<button onclick="start()">Start!</button>`;
 
-    document.getElementById("quizBody").innerHTML = quizContent;
+document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-//Time deducted if answer is wrong
-function incorrectResponse() {
-    timeLeft -= 15; 
-    next();
+//deducts 5 seconds for every incorrect response
+function incorrect() {
+timeLeft -= 5; 
+next();
 }
 
-//Add 5 points for every answer that is correct
-function correctResponse() {
-    score += 5;
-    next();
+//adds 5 points to each correct answer
+function correct() {
+score += 5;
+next();
 }
 
-// Moving through the series of questions. 
+//Series of questions asked 
 function next() {
-    currentQuestion++;
+currentQuestion++;
 
-    if (currentQuestion > questions.length - 1) {
-        endQuiz();
-        return;
+if (currentQuestion > questions.length - 1) {
+    endGame();
+    return;
+}
+
+var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+    var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+    if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+        buttonCode = buttonCode.replace("[ANS]", "correct()");
+    } else {
+        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
     }
-
-    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
-
-    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
-        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
-        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
-        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
-            buttonCode = buttonCode.replace("[ANS]", "correct()");
-        } else {
-            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
-        }
-        quizContent += buttonCode
-    }
+    quizContent += buttonCode
+}
 
 
-    document.getElementById("quizBody").innerHTML = quizContent;
+document.getElementById("quizBody").innerHTML = quizContent;
 }
