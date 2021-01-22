be27043a37c75bf2b6d9ec090b89edaee8caa9b4
Ingrid Lyublinsky <ingrid.lyub@gmail.com>
2021-01-21 21:02:11 -0800

    Finished code for local storage

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -71,3 +71,15 @@ function setScore() {
     localStorage.setItem("highscoreName",  document.getElementById('name').value);
     getScore();
 }
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
